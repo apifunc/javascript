e03bf9cacc2c97c15e0348a2502398b545be7b0a
tom <tom@sapletta.com>
2020-07-17 15:04:50 +0200

    if not Null

--- src/is-array.js
+++ src/is-array.js
@@ -7,7 +7,7 @@ jlogs('exist?','isArray');
  */
 function isArray(val) {
     return val !== null &&
-        (val instanceof Array)
+        (val instanceof Array && Object.keys(val).length > 0)
     // obj.constructor.toString().indexOf("Array") != -1
         ;
 }
