999f7580d5877381444e956bf071d2c216c3b8b7
tom-sapletta-com <tom@sapletta.com>
2021-10-30 15:24:06 +0200

    preparation before unittest

--- test/is-number.test.js
+++ test/is-number.test.js
@@ -1,11 +1,17 @@
 const isNumber = require('../src/is-number');
 
-test('Check num', () => {
+test('Check positive and zero number', () => {
 	expect(isNumber( 1)).toBe( true);
 	expect(isNumber(  0)).toBe( true);
-	expect(isNumber( -1)).toBe( true);
+});
+
+test('Check not numbers', () => {
 	expect(isNumber( "test")).toBe( false);
 	expect(isNumber( null)).toBe( false);
 	expect(isNumber( {})).toBe( false);
 	expect(isNumber( [])).toBe( false);
+});
+
+test('Check negative number', () => {
+	expect(isNumber( -1)).toBe( true);
 });
