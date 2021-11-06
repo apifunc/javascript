69a015d508d4831e2197f50d81a64424a5b5fcfa
tom-sapletta-com <tom@sapletta.com>
2021-11-06 11:35:51 +0100

    more about jest and isUrl() unittests

--- test/is-url.test.js
+++ test/is-url.test.js
@@ -1,5 +1,15 @@
 const isUrl = require('../src/is-url');
 
-test('Is the url with http prefixs', () => {
+test('Is the url with http prefix', () => {
     expect(isUrl("http://domain.com")).toBe(true);
+});
+
+
+test('Is the url with https prefix', () => {
+	expect(isUrl("https://domain.com")).toBe(true);
+});
+
+
+test('Is the url with // prefix', () => {
+	expect(isUrl("//domain.com")).toBe(true);
 });
