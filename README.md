2e672c0069af1262301f8458c10bd30a9f24d1d3
tom-sapletta-com <tom@sapletta.com>
2021-11-08 20:04:42 +0100

    unit test description and some examples

--- README.md
+++ README.md
@@ -10,6 +10,13 @@ Jest is a JavaScript test runner, that is, a JavaScript library for creating, ru
 
 Jest ships as an NPM package, you can install it in any JavaScript project. Jest is one of the most popular test runner these days, and the default choice for React projects.
 
+### Why unittest?
+
+the reason why wy are testing is to give more quality of our sourcecode
+
+We are doing it for our customers, that they can be sure that the code ist tested.
+
+
 ## START
 
     npm install
--- test/is-object.test.js
+++ test/is-object.test.js
@@ -1,5 +1,14 @@
 const isObject = require('../src/is-object');
 
 test('Check if we are passing an object', () => {
-	expect(isObject(typeof val)).toBe( true );
+	expect( isObject({'':''}) ).toBe( true );
+	expect( isObject({'0':''}) ).toBe( true );
+	expect( isObject({'0':'0'}) ).toBe( true );
+	expect( isObject({'1':'1'}) ).toBe( true );
+});
+
+test('What is not recognized as an object', () => {
+	expect( isObject() ).toBe( false );
+	expect( isObject('') ).toBe( false );
+	expect( isObject({}) ).toBe( false );
 });
